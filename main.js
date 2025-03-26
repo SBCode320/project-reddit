c7ade8978f2f703c6d06ebff668ce02f9a78227f
Shiva Bodhireddy <{ID}+{username}@users.noreply.github.com>
2025-03-26 16:47:09 -0700

    Add remove post and comment buttons

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 //Event listener for clicking on submit button
 document.getElementById('submit').addEventListener('click', function () {
-  var name = document.getElementById('name').value;
-  var text = document.getElementById('message').value;
+  const name = document.getElementById('name').value;
+  const text = document.getElementById('message').value;
 
-  var postsDiv = document.querySelector('.posts');
+  const postsDiv = document.querySelector('.posts');
 
-  var newPostDiv = document.createElement('div');
+  //Create wrapper for individual post
+  const newPostDiv = document.createElement('div');
 
-  var newPostP = document.createElement('p');
-  var newPostNode = document.createTextNode(`${text} - Posted By: ${name}`);
+  //Create <p> and text node for post 
+  const newPostP = document.createElement('p');
+  const newPostNode = document.createTextNode(`${text} - Posted By: ${name}`);
   newPostP.appendChild(newPostNode);
 
-  var newPostHR = document.createElement('hr');
+  //Create remove button
+  const removeButton = document.createElement('button'); 
+  removeButton.textContent = 'Remove Post'; 
 
+  //Create comments button
+  const commentButton = document.createElement('button'); 
+  commentButton.textContent = 'See Comments'; 
+
+  //Create <hr> 
+  const newPostHR = document.createElement('hr');
+
+  //Append the remove link, post, and hr to the post div
+  newPostDiv.append(removeButton); 
+  newPostDiv.append(commentButton); 
   newPostDiv.append(newPostP);
   newPostDiv.append(newPostHR);
 
+  //Append the post to postDiv
   postsDiv.append(newPostDiv);
+
 
 })
